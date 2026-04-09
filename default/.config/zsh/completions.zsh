c546a6ad471201e355fbdf9945e22b6fa188ebd7
winetree94 <me@winetree94.com>
2026-04-09 10:40:50 +0900

    feat: add opencode shell completion support

--- default/.config/zsh/completions.zsh
+++ default/.config/zsh/completions.zsh
@@ -3,4 +3,5 @@ maybe_eval devsync autocomplete zsh
 maybe_eval docker completion zsh
 maybe_eval kubectl completion zsh
 maybe_eval talosctl completion zsh
+maybe_eval opencode completion
 # maybe_eval bw completion --shell zsh
