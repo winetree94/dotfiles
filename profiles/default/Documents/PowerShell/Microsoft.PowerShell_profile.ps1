8e3c99cb98b4ae0204dc5baf015bf32865f005a5
winetree94 <me@winetree94.com>
2026-06-16 19:02:54 +0900

    feat: windows powershell config

--- profiles/default/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ profiles/default/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
-(&mise activate pwsh) | Out-String | Invoke-Expression
-
 function claude { claude.exe --allow-dangerously-skip-permissions @args }
 
 function codex { codex.exe --dangerously-bypass-approvals-and-sandbox @args }
