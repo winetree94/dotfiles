30aafb59855d53bda710458949c02eadacfc8c74
winetree94 <me@winetree94.com>
2026-06-17 14:21:35 +0900

    chore: add vivident AWS profile

--- profiles/default/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ profiles/default/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
 function claude { claude.exe --allow-dangerously-skip-permissions @args }
 
 function codex { codex.exe --dangerously-bypass-approvals-and-sandbox @args }
+
+$env:AWS_PROFILE = "vivident"
+
