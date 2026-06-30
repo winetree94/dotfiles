b4c5635675a888ddf5d8f87ad583afa90e89ae9c
winetree94 <me@winetree94.com>
2026-06-30 15:56:04 +0900

    chore: refresh default profile tooling

--- profiles/default/.config/nvim/lua/plugins/tree-sitter.lua
+++ profiles/default/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,34 +1,35 @@
 vim.pack.add({
-  {
-    src = "https://github.com/nvim-treesitter/nvim-treesitter",
-    version = "main",
-  },
-  {
-    src = "https://github.com/davidmh/mdx.nvim",
-    version = "main",
-  },
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		version = "main",
+	},
+	{
+		src = "https://github.com/davidmh/mdx.nvim",
+		version = "main",
+	},
 }, { confirm = false, load = false })
 
 -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
 require("nvim-treesitter").install({
-  "lua",
-  "astro",
-  "javascript",
-  "jsx",
-  "typescript",
-  "tsx",
-  "markdown",
-  "markdown_inline",
-  "python",
+	"lua",
+	"astro",
+	"javascript",
+	"jsx",
+	"typescript",
+	"tsx",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"dart",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  callback = function(args)
-    local ft = args.match
-    local lang = vim.treesitter.language.get_lang(ft) or ft
-    local ok = pcall(vim.treesitter.language.inspect, lang)
-    if ok then
-      vim.treesitter.start(args.buf, lang)
-    end
-  end,
+	callback = function(args)
+		local ft = args.match
+		local lang = vim.treesitter.language.get_lang(ft) or ft
+		local ok = pcall(vim.treesitter.language.inspect, lang)
+		if ok then
+			vim.treesitter.start(args.buf, lang)
+		end
+	end,
 })
