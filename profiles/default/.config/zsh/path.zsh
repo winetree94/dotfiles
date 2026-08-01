194e14c4601673349d72f580c9b8050fd23a9a86
winetree94 <me@winetree94.com>
2026-08-02 00:57:36 +0900

    chore: update zsh configuration with android SDK paths and dart completion

--- profiles/default/.config/zsh/path.zsh
+++ profiles/default/.config/zsh/path.zsh
@@ -3,13 +3,25 @@ export PATH="$HOME/.cache/.bun/bin:$PATH"
 export PATH="$HOME/.bun/bin:$PATH"
 
 export PATH="/home/winetree94/.cache/.bun/bin:$PATH"
+
+# android
 export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-amd64"
 export ANDROID_SDK_ROOT="$HOME/Android/Sdk"
+export PATH="$ANDROID_SDK_ROOT/platform-tools:$PATH"
+export PATH="$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH"
+
+# brew
 export PATH="/home/linuxbrew/.linuxbrew/opt/libpq/bin:$PATH"
 
+# vivident
 export VIVIDENT_KEYSTORE_DIR=$HOME/.vivident
+
+# git
 export GPG_TTY=$(tty)
+
 export XDG_CONFIG_HOME=$HOME/.config
+
+# pnpm
 export PNPM_HOME="/home/winetree94/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME/bin:"*) ;;
--- profiles/default/.zshrc
+++ profiles/default/.zshrc
@@ -1,2 +1,8 @@
 source ~/.config/zsh/entry.zsh
 
+
+## [Completion]
+## Completion scripts setup. Remove the following line to uninstall
+[[ -f /home/winetree94/.config/.dart-cli-completion/zsh-config.zsh ]] && . /home/winetree94/.config/.dart-cli-completion/zsh-config.zsh || true
+## [/Completion]
+
