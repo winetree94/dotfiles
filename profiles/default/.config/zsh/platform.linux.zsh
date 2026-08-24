77fcfd0aa29715129dc7365907ef22a950659abb
winetree94 <me@winetree94.com>
2026-08-24 17:24:33 +0900

    dotweave: sync configuration

---
+++ profiles/default/.config/zsh/platform.linux.zsh
@@ -0,0 +1,2 @@
+# for ubuntu 26 compatibility
+export PLAYWRIGHT_HOST_PLATFORM_OVERRIDE=ubuntu24.04-x64
--- profiles/default/.config/zsh/platform.wsl.zsh
+++ profiles/default/.config/zsh/platform.wsl.zsh
@@ -1,3 +1,6 @@
+# for ubuntu 26 compatibility
+export PLAYWRIGHT_HOST_PLATFORM_OVERRIDE=ubuntu24.04-x64
+
 if [ -n "$DISPLAY" ] || [ -n "$WAYLAND_DISPLAY" ]; then
   pgrep -x ibus-daemon >/dev/null || ibus-daemon -drx >/dev/null 2>&1 &
 fi
