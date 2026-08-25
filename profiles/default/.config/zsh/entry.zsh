9c9350cf002a37493c79a3fbefe603ac0bf2381f
winetree94 <me@winetree94.com>
2026-08-25 10:23:44 +0900

    dotweave: sync configuration

--- profiles/default/.config/zsh/entry.zsh
+++ profiles/default/.config/zsh/entry.zsh
@@ -2,6 +2,7 @@ ZSH_CONFIG_DIR=$HOME/.config/zsh
 
 source $ZSH_CONFIG_DIR/utils.zsh
 source_if_exists $ZSH_CONFIG_DIR/path.zsh
+setopt typesetsilent
 source_if_exists $ZSH_CONFIG_DIR/antidote.zsh
 source_if_exists $ZSH_CONFIG_DIR/completions.zsh
 source_if_exists $ZSH_CONFIG_DIR/configs.zsh
