a267b47544d4c409f1b92e098e4b8b088ef812d9
winetree94 <me@winetree94.com>
2026-09-22 17:15:06 +0900

    dotweave: sync configuration

--- profiles/default/.config/zsh/completions.zsh
+++ profiles/default/.config/zsh/completions.zsh
@@ -5,5 +5,3 @@ maybe_eval kubectl completion zsh
 maybe_eval talosctl completion zsh
 maybe_eval cf complete zsh
 [[ -f /home/winetree94/.config/.dart-cli-completion/zsh-config.zsh ]] && . /home/winetree94/.config/.dart-cli-completion/zsh-config.zsh || true
-if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
-# maybe_eval bw completion --shell zsh
